381f76642ea6ff7a860b56405262ea7d155f6670
Lucas Connors <lucas.revolutiontech@gmail.com>
2017-07-06 17:08:15 -0700

    PEP8: Remove duplicate definition of execute_from_command_line

--- test_project/runtests.py
+++ test_project/runtests.py
@@ -10,8 +10,8 @@ if len(sys.argv) == 2:
 
 
 if __name__ == "__main__":
-    from django.core.management import execute_from_command_line
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
     from django.core.management import execute_from_command_line
+
     execute_from_command_line(sys.argv)
