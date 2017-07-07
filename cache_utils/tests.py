9cabe627fa6f5311cdafff33386f91e434b730a7
Lucas Connors <lucas.revolutiontech@gmail.com>
2017-07-06 17:26:24 -0700

    Remove commented out code

--- cache_utils/tests.py
+++ cache_utils/tests.py
@@ -167,19 +167,6 @@ class DecoratorTest(ClearMemcachedTest):
         self.assertEqual(my_func(u"Ы"*500), u"5"+u"Ы"*500)
         self.assertEqual(my_func(u"Ы"*500), u"5"+u"Ы"*500)
 
-#    Currently lacks support UTF8
-#     def test_utf8_args(self):
-#         @cached(60)
-#         def func(utf8_array, *args):
-#             return utf8_array
-#         func([u'Василий'.encode('utf8')], u'Петрович'.encode('utf8'))
-#
-#     def test_utf8_repr(self):
-#         @cached(60)
-#         def func(param):
-#             return param
-#         func(Store())
-
     def test_key_override(self):
         """
         Test the cache key naming.
