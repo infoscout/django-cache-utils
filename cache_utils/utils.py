fc5eb214210811f5917c5cb586534041359071de
Cemre Yolveren <cyolveren@gmail.com>
2023-04-19 13:08:17 -0700

    Update utils.py

--- cache_utils/utils.py
+++ cache_utils/utils.py
@@ -93,4 +93,4 @@ def _cache_key(func_name, func_type, args, kwargs):
     else:
         args_string = _args_to_unicode(args[1:], kwargs)
 
-    return '[cached]%s(%s)%s:%s' % (func_name, args_string,)
+    return '[cached]%s(%s)' % (func_name, args_string,)
