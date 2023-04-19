8cc2e6425ec6187027c6be77d3f3993df91b0a1b
Cemre Yolveren <cyolveren@gmail.com>
2023-04-19 14:02:05 -0700

    Update utils.py

--- cache_utils/utils.py
+++ cache_utils/utils.py
@@ -75,12 +75,12 @@ def stringify_args(args, kwargs, object_attrs: None) -> Tuple[list, dict]:
             return [stringify(e) for e in obj]
         elif isinstance(obj, dict):
             sorted_items = sorted(obj.items())
-            return "{" + ", ".join(f"{k!r}: {stringify(v)}" for k, v in sorted_items) + "}"
+            return "{" + ", ".join("{!r}: {}".format(k, stringify(v)) for k, v in sorted_items) + "}"
         elif hasattr(obj, '__dict__'):
             obj_str = obj.__class__.__name__
             attrs = {attr: str(getattr(obj, attr, None)) for attr in object_attrs.get(obj.__class__, [])}
             sorted_attrs = OrderedDict(sorted(attrs.items()))
-            return obj_str + "{" + ", ".join(f"{k!r}: {v!r}" for k, v in sorted_attrs.items()) + "}"
+            return obj_str + "{" + ", ".join("{!r}: {!r}".format(k, v) for k, v in sorted_attrs.items()) + "}"
         else:
             return str(obj)
 
