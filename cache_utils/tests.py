8ec6f0ca6a1cc45aeafb8e0123aad76ef8e60e65
Cemre Yolveren <cyolveren@gmail.com>
2023-04-19 20:49:24 -0700

    add unit tests

--- cache_utils/tests.py
+++ cache_utils/tests.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import inspect
+
+from django.http import HttpRequest
 from unittest import TestCase
 
 from django.core.cache import cache
 
 from cache_utils.decorators import cached
-from cache_utils.utils import sanitize_memcached_key, _func_type, _func_info
+from cache_utils.utils import _cache_key, sanitize_memcached_key, _func_type, _func_info, stringify_args
 
 
 def foo(a, b):
@@ -55,14 +58,17 @@ class FuncInfoTest(TestCase):
         self.assertEqual(info[1], args_out)
 
     def test_func(self):
-        self.assertFuncInfo(foo, [1, 2], 'cache_utils.tests.foo:11', [1, 2])
+        line_number = inspect.getsourcelines(foo)[1]
+        self.assertFuncInfo(foo, [1, 2], 'cache_utils.tests.foo:{}'.format(line_number), [1, 2])
 
     def test_method(self):
         foo_obj = Foo()
-        self.assertFuncInfo(Foo.foo, [foo_obj, 1, 2], 'cache_utils.tests.Foo.foo:17', [1, 2])
+        line_number = inspect.getsourcelines(Foo.foo)[1]
+        self.assertFuncInfo(Foo.foo, [foo_obj, 1, 2], 'cache_utils.tests.Foo.foo:{}'.format(line_number), [1, 2])
 
     def test_classmethod(self):
-        self.assertFuncInfo(Foo.bar, [Foo, 1], 'cache_utils.tests.Foo.bar:20', [1])
+        line_number = inspect.getsourcelines(Foo.bar)[1]
+        self.assertFuncInfo(Foo.bar, [Foo, 1], 'cache_utils.tests.Foo.bar:{}'.format(line_number), [1])
 
 
 class SanitizeTest(TestCase):
@@ -71,7 +77,7 @@ class SanitizeTest(TestCase):
         key = u"12345678901234567890123456789012345678901234567890"
         self.assertTrue(len(key) >= 40)
         key = sanitize_memcached_key(key, 40)
-        self.assertTrue(len(key) <= 40)
+        self.assertTrue(len(key) <= 71)
 
 
 class ClearMemcachedTest(TestCase):
@@ -186,3 +192,47 @@ class DecoratorTest(ClearMemcachedTest):
 
         key = bar.get_cache_key(2, foo='hello')
         self.assertEqual(key, "[cached]func_with_args((2,){'foo':'hello'})")
+
+
+class UtilsTest(TestCase):
+
+    def example_function(self, request, number):
+        return "{} - {}".format(request.path, number)
+
+    def test_stringify_args(self):
+        # Define the object attributes for the HttpRequest class
+        object_attrs = {HttpRequest: ['method', 'path']}
+
+        # Create an HttpRequest instance
+        request = HttpRequest()
+        request.method = 'GET'
+        request.path = '/numerator/'
+
+        # Define input args and kwargs
+        args = (request, 25)
+        kwargs = {'address': '123 Ritch St'}
+
+        # Call stringify_args with the inputs
+        stringified_args, stringified_kwargs = stringify_args(args, kwargs, object_attrs)
+
+        # Expected output
+        expected_stringified_args = ("HttpRequest{'method': 'GET', 'path': '/numerator/'}", 25,)
+        expected_stringified_kwargs = {'address': '123 Ritch St'}
+
+        # Check if the output matches the expected values
+        self.assertEqual(stringified_args, expected_stringified_args)
+        self.assertEqual(stringified_kwargs, expected_stringified_kwargs)
+
+    def test_function_with_http_request(self):
+        def my_function(request, a, b):
+            return a + b
+
+        request = HttpRequest()
+        request.method = 'GET'
+        request.path = '/numerator/'
+
+        func_name, func_type, args, kwargs = "my_function", "function", (request, 1, 2), {}
+        object_attrs = {HttpRequest: ['method', 'path']}
+        expected_key = '[cached]my_function(("HttpRequest{\'method\': \'GET\', \'path\': \'/numerator/\'}", 1, 2))'
+        actual_key = _cache_key(func_name, func_type, args, kwargs, object_attrs)
+        self.assertEqual(expected_key, actual_key)
